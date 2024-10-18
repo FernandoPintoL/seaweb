db16a1d19a902a5b797675eca6750b17de3c7454
Fernando Pinto Lino <pintolinofernando@gmail.com>
2024-10-17 21:33:52 -0400

    enlaces.

--- resources/js/Componentes/MenuLateral.vue
+++ resources/js/Componentes/MenuLateral.vue
@@ -174,7 +174,7 @@ const logout = () => {
                             $page.props.user.permissions_roles.includes('ROLE.LISTAR') ||
                             $page.props.user.permissions_roles.includes('ROLE.MOSTRAR')">
                             <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-300"
-                                :href="route('users.index')">
+                                :href="route('roles.index')">
                                 <i class="fa-regular fa-user"></i>
                                 Roles
                             </a>
@@ -185,7 +185,7 @@ const logout = () => {
                             $page.props.user.permissions_roles.includes('PERMISSION.LISTAR') ||
                             $page.props.user.permissions_roles.includes('PERMISSION.MOSTRAR')">
                             <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-300"
-                                :href="route('users.index')">
+                                :href="route('permissions.index')">
                                 <i class="fa-regular fa-user"></i>
                                 Permisos
                             </a>
